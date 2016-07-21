4b4ef5597a711c4d82ade72323364cbd7d2a500a
Sean Shannon <seans887@gmail.com>
2016-07-21 09:35:09 -0500

    Initial commit with webpack config set up

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+
+var debug = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: "./src/script.js",
+  output: {
+    path: __dirname + "/public/javascripts/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
